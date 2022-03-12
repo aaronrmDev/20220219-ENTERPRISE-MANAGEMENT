04344a5f2c0c1e8bea6136ac91522a6c7154b2a4
Juanjo Sequera Fernández <juan.sequera.fernandez.business@outlook.com>
2022-03-12 18:28:26 -0300

    new project

--- 20220312-GUI/Program.cs
+++ 20220312-GUI/Program.cs
@@ -4,3 +4,5 @@ using _20220219_ENTERPRISE_MANAGEMENT.com.logic.project;
 Console.WriteLine("Hello, World!");
 
 Project project = new LongPeriod(1, "Projectx", new DateTime().Date, 5.5);
+
+Project project2 = new LongPeriod(3, "Projectx2", new DateTime().Date, 5.5);
