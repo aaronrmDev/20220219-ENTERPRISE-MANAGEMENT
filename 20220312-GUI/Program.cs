24668820a222ba3f139f4ba51977a83cef640859
Sebastian Arambulo <earambulo.sa@gmail.com>
2022-03-12 18:37:41 -0300

    add feature

--- 20220312-GUI/Program.cs
+++ 20220312-GUI/Program.cs
@@ -1,6 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using _20220219_ENTERPRISE_MANAGEMENT.com.logic.member;
 using _20220219_ENTERPRISE_MANAGEMENT.com.logic.project;
 
 Console.WriteLine("Hello, World!");
 
 Project project = new LongPeriod(2, "new", new DateTime().Date, 123);
+
+Member member = new Member();
